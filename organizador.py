81419501fe4b5c65cfbf68ff58931478629316e8
DenisPablo <denispablo17@gmail.com>
2023-11-18 18:39:36 -0300

    Servicio de Windows

--- organizador.py
+++ organizador.py
@@ -1,10 +1,54 @@
 # El script analiza un directorio en concreto y organizara los archivos en su interior siguiendo las siguientes reglas:
 # si el archivo no tiene una extension contemplada por el script ira a la carpeta otros y las carpetas que no esten dentro del diccionario tambien iran a otros
 
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import sys
 from pathlib import Path
 from shutil import move
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+class OrganizadorService(win32serviceutil.ServiceFramework):
+    _svc_name_ = 'OrganizadorArchivosService'
+    _svc_display_name_ = 'Organizador de Archivos Service'
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(120)
+        self.is_alive = True
+
+    def SvcStop(self, args):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+        self.is_alive = False
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(
+            servicemanager.EVENTLOG_INFORMATION_TYPE,
+            servicemanager.PYS_SERVICE_STARTED,
+            (self._svc_name_, '')
+        )
+        self.main()
+
+    def main(self):
+        ruta_directorio_raiz = Path(r'C:\Users\denis\OneDrive\Documentos')
+        handler = Organizador_de_archivos_handler(ruta_directorio_raiz)
+        observer = Observer()
+        observer.schedule(handler, path=ruta_directorio_raiz, recursive=False)
+        observer.start()
+
+        try:
+            while self.is_alive:
+                    observer.join(timeout=1)
+        except KeyboardInterrupt:
+                observer.stop()
+
+        observer.join()
 
 class Organizador_de_archivos_handler(FileSystemEventHandler):
     def __init__(self, directorio_raiz):
@@ -12,9 +56,6 @@ class Organizador_de_archivos_handler(FileSystemEventHandler):
         self.directorio_raiz = directorio_raiz
 
     def on_created(self, event):
-        if event.is_directory:
-            return  # Ignorar directorios creados
-
         print(f'Nuevo archivo detectado: {event.src_path}')
         organizador_archivos(self.directorio_raiz)
 
@@ -65,16 +106,10 @@ def organizador_archivos(directorio_raiz):
             print(f'Error al mover {elemento.name}: {e}')
 
 if __name__ == '__main__':
-        ruta_directorio_raiz = Path(r'C:\Users\denis\OneDrive\Documentos')
-        handler = Organizador_de_archivos_handler(ruta_directorio_raiz)
-        observer = Observer()
-        observer.schedule(handler, path=ruta_directorio_raiz, recursive=False)
-        observer.start()
 
-        try:
-            print(f'Observando el directorio: {ruta_directorio_raiz}')
-            observer.join(timeout=1)
-        except KeyboardInterrupt:
-            observer.stop()
-
-        observer.join()
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(OrganizadorService)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(OrganizadorService)
