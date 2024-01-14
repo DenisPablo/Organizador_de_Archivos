316b5ac6633a4a2e47607a1fabb3ecb96dd64cce
DenisPablo <denispablo17@gmail.com>
2024-01-13 23:03:18 -0300

    Fix

--- organizador.py
+++ organizador.py
@@ -3,20 +3,6 @@
 
 from pathlib import Path
 from shutil import move
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-class Organizador_de_archivos_handler(FileSystemEventHandler):
-    def __init__(self, directorio_raiz):
-        super().__init__()
-        self.directorio_raiz = directorio_raiz
-
-    def on_created(self, event):
-        if event.is_directory:
-            return  # Ignorar directorios creados
-
-        print(f'Nuevo archivo detectado: {event.src_path}')
-        organizador_archivos(self.directorio_raiz)
 
 def organizador_archivos(directorio_raiz):
     carpetas_extensiones = {
@@ -48,7 +34,7 @@ def organizador_archivos(directorio_raiz):
 
 
     for elemento in directorio_raiz.iterdir():
-        if elemento.is_file():
+        if elemento.is_file() and elemento.name == "organizador.py" :
             carpeta_destino = carpetas_extensiones.get(elemento.suffix, carpeta_otros)
         elif elemento.is_dir() and elemento.name not in set(carpetas_extensiones.values()) and elemento.name != 'Otros':
             carpeta_destino = carpeta_otros
@@ -65,16 +51,5 @@ def organizador_archivos(directorio_raiz):
             print(f'Error al mover {elemento.name}: {e}')
 
 if __name__ == '__main__':
-        ruta_directorio_raiz = Path(r'C:\Users\denis\OneDrive\Documentos')
-        handler = Organizador_de_archivos_handler(ruta_directorio_raiz)
-        observer = Observer()
-        observer.schedule(handler, path=ruta_directorio_raiz, recursive=False)
-        observer.start()
-
-        try:
-            print(f'Observando el directorio: {ruta_directorio_raiz}')
-            observer.join(timeout=1)
-        except KeyboardInterrupt:
-            observer.stop()
-
-        observer.join()
+        ruta_directorio_raiz = Path.cwd()
+        organizador_archivos(ruta_directorio_raiz)
